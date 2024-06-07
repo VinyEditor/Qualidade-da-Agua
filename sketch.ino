d89863712cc3390a4aa0d7f4ecd8e4f84dfa9144
Vinicius Cruzeiro <111810869+VinyEditor@users.noreply.github.com>
2024-06-07 19:42:34 -0300

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -14,13 +14,16 @@
 #define PIN_OXIGENIO 35 // Pino do sensor de oxigênio dissolvido
 
 // Definições de credenciais do Thinger.io
-#define THINGER_DEVICE_ID "seu_device_id"
-#define THINGER_DEVICE_CREDENTIAL "seu_device_credential"
-#define THINGER_BUCKET_ID "seu_bucket_id"
+#define USERNAME "VInyEditor" // Substitua pelo seu nome de usuário do Thinger.io
+#define DEVICE_ID "ESP32"
+#define DEVICE_CREDENTIAL "mudar.520"
+#define BUCKET_ID "bES32" // Substitua pelo seu ID de bucket no Thinger.io
 
-ThingerESP32 thing(THINGER_DEVICE_ID, THINGER_DEVICE_CREDENTIAL);
+ThingerESP32 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
 void setup() {
+  Serial.begin(115200);
+
   // Inicializar sensores
   pinMode(PIN_PH, INPUT);
   pinMode(PIN_TEMPERATURA, INPUT);
@@ -28,36 +31,67 @@ void setup() {
   pinMode(PIN_OXIGENIO, INPUT);
 
   // Conectar ao Wi-Fi
-  WiFi.begin("seu_ssid", "seu_senha");
-  while (WiFi.status()!= WL_CONNECTED) {
+  WiFi.begin("Wokwi-GUEST");
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Conectando ao Wi-Fi...");
   }
 
   Serial.println("Conectado ao Wi-Fi!");
-  thing.begin();
+  thing.add_wifi("Wokwi-GUEST");
+
+  // Definindo recursos para cada sensor
+  thing["ph"] >> [](pson & out){
+    int phValue = analogRead(PIN_PH);
+    float ph = phValue * 3.3 / 4095; // Ajuste para 0-3.3V
+    out = ph;
+  };
+
+  thing["temperatura"] >> [](pson & out){
+    int temperaturaValue = analogRead(PIN_TEMPERATURA);
+    float temperatura = temperaturaValue * 3.3 / 4095; // Ajuste para 0-3.3V
+    out = temperatura;
+  };
+
+  thing["turbidez"] >> [](pson & out){
+    int turbidezValue = analogRead(PIN_TURBIDEZ);
+    float turbidez = turbidezValue * 3.3 / 4095; // Ajuste para 0-3.3V
+    out = turbidez;
+  };
+
+  thing["oxigenio"] >> [](pson & out){
+    int oxigenioValue = analogRead(PIN_OXIGENIO);
+    float oxigenio = oxigenioValue * 3.3 / 4095; // Ajuste para 0-3.3V
+    out = oxigenio;
+  };
 }
 
 void loop() {
-  // Gerar valores aleatórios para os sensores
+  thing.handle();
+
+  // Gerar novos valores aleatórios a cada 10 segundos
+  delay(10000);
+
+  // Fazer leituras e enviar para o Thinger.io
   int phValue = random(0, 1024);
+  float ph = phValue * 3.3 / 1023; // Ajuste para 0-3.3V
+
   int temperaturaValue = random(0, 1024);
+  float temperatura = temperaturaValue * 3.3 / 1023; // Ajuste para 0-3.3V
+
   int turbidezValue = random(0, 1024);
+  float turbidez = turbidezValue * 3.3 / 1023; // Ajuste para 0-3.3V
+
   int oxigenioValue = random(0, 1024);
+  float oxigenio = oxigenioValue * 3.3 / 1023; // Ajuste para 0-3.3V
 
-  // Converter valores para unidades adequadas
-  float ph = phValue * 3.3 / 4095;
-  float temperatura = temperaturaValue * 3.3 / 4095;
-  float turbidez = turbidezValue * 3.3 / 4095;
-  float oxigenio = oxigenioValue * 3.3 / 4095;
+  // Criando um objeto pson para enviar os dados
+  pson data;
+  data["ph"] = ph;
+  data["temperatura"] = temperatura;
+  data["turbidez"] = turbidez;
+  data["oxigenio"] = oxigenio;
 
-  // Enviar dados para o Thinger.io
-  thing.add("ph", ph);
-  thing.add("temperatura", temperatura);
-  thing.add("turbidez", turbidez);
-  thing.add("oxigenio", oxigenio);
-
-  thing.send();
-
-  delay(10000); // Enviar dados a cada 10 segundos
+  // Enviando dados para o Thinger.io
+  thing.write_bucket(BUCKET_ID, data);
 }
